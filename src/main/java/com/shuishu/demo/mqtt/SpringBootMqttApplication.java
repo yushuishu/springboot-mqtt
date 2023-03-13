19129c1fa1c45e6dbf9f35cd6b55e92616d5c481
谁书-ss <yushuishu@qq.com>
2023-03-13 22:40:03 +0800

    feat:mqtt

---
+++ src/main/java/com/shuishu/demo/mqtt/SpringBootMqttApplication.java
@@ -0,0 +1,22 @@
+package com.shuishu.demo.mqtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author ：谁书-ss
+ * @date   ： 2023-03-13 21:59
+ * @IDE    ：IntelliJ IDEA
+ * @motto  ：ABC(Always Be Coding)
+ * <p></p>
+ * @description ：mqtt协议的应用，MQTTX
+ */
+@SpringBootApplication
+public class SpringBootMqttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMqttApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shuishu/demo/mqtt/SpringBootMqttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuishu.demo.mqtt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMqttApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
